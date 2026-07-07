2cfdf55bf16c02c5f8a7a426047b137fa32a8094
Stepan Demianenko <42498337+sneri12pic@users.noreply.github.com>
2026-07-07 23:16:22 +0100

    Add: baseball-game - submission-4

---
+++ Data Structures & Algorithms/baseball-game/submission-4.py
@@ -0,0 +1,29 @@
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        
+        res = []
+        i = 0
+        while i < len(operations):
+            cur = operations[i]
+            
+            if cur == "D":
+                cur = res[-1] # last num in res
+                doubledCur = int(cur) * 2
+                res.append(str(doubledCur)) 
+
+            elif cur == "C":
+                res.pop()
+            elif cur == "+": 
+                s2 = res.pop() # save the num and pop again since we need to add sum of 2
+                s1 = res[-1] # shifted to -1 index
+
+                newScore = int(s1) + int(s2)
+                res.append(s2)
+                res.append(str(newScore)) # add a new score that "+" have created
+            else:
+                res.append(cur)
+            i += 1
+
+        answer = 0
+        for num in res: answer += int(num)    
+        return answer
